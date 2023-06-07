362eb1005f57dac23c3f8b2690f9cffdc9dad995
KyleHS <k15156@yahoo.com>
2023-06-07 13:23:41 -0600

    first commit.

---
+++ app.js
@@ -0,0 +1,11 @@
+const houseCats = 
+    {Winston:1, Yoshi:5, Fennekin:3, Daphne:4} //Start with an array and you can place multiple objects within
+
+
+houseCats["Winston"]
+
+//1
+
+houseCats.Daphne
+
+//4   These are ways to call these
